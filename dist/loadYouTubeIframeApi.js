0adeddc6b5e4a321a03b5eafff3812bd3e9b1367
Ian Serpa <i.serpa@elsevier.com>
2020-08-04 17:16:18 +0200

    chore: make it more modern

---
+++ dist/loadYouTubeIframeApi.js
@@ -0,0 +1,32 @@
+import load from 'load-script';
+export default (emitter => {
+  /**
+   * A promise that is resolved when window.onYouTubeIframeAPIReady is called.
+   * The promise is resolved with a reference to window.YT object.
+   */
+  const iframeAPIReady = new Promise(resolve => {
+    if (window.YT && window.YT.Player && window.YT.Player instanceof Function) {
+      resolve(window.YT);
+      return;
+    } else {
+      const protocol = window.location.protocol === 'http:' ? 'http:' : 'https:';
+      load(protocol + '//www.youtube.com/iframe_api', error => {
+        if (error) {
+          emitter.trigger('error', error);
+        }
+      });
+    }
+
+    const previous = window.onYouTubeIframeAPIReady; // The API will call this function when page has finished downloading
+    // the JavaScript for the player API.
+
+    window.onYouTubeIframeAPIReady = () => {
+      if (previous) {
+        previous();
+      }
+
+      resolve(window.YT);
+    };
+  });
+  return iframeAPIReady;
+});
